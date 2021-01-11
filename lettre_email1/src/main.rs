41e51d77e147c1b4c67f9338ee2e6c91f42fbac3
bouzuya <m@bouzuya.net>
2021-01-11 19:41:05 +0900

    add lettre example

--- lettre_email1/src/main.rs
+++ lettre_email1/src/main.rs
@@ -1,3 +1,26 @@
+use lettre::{
+    smtp::{error::Error, response::Response},
+    ClientSecurity, SendableEmail, SmtpClient, SmtpTransport, Transport,
+};
+use lettre_email::Email;
+
 fn main() {
-    println!("Hello, world!");
+    let email: Email = Email::builder()
+        .to(("m@bouzuya.net", "bouzuya"))
+        .from("m@bouzuya.net")
+        .subject("SUBJECT")
+        .text("Hello lettre.")
+        .build()
+        .unwrap();
+    let sendable_email: SendableEmail = email.into();
+
+    // let smtp_client = SmtpClient::new_unencrypted_localhost().unwrap();
+    let smtp_client: SmtpClient =
+        SmtpClient::new(("localhost", 3025), ClientSecurity::None).unwrap();
+    let mut smtp_transport: SmtpTransport = smtp_client.transport();
+    let result: Result<Response, Error> = smtp_transport.send(sendable_email);
+    match result {
+        Ok(resp) => println!("Ok: {:?}", resp),
+        Err(err) => println!("Err: {:?}", err),
+    }
 }
