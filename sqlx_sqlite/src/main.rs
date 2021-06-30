8dc9e0769db4a7b49ad0cb6b1d8344ecdaa3fa88
bouzuya <m@bouzuya.net>
2021-06-30 18:58:06 +0900

    sqlx_sqlite: add tests

--- sqlx_sqlite/src/main.rs
+++ sqlx_sqlite/src/main.rs
@@ -32,6 +32,107 @@ mod tests {
     }
 
     #[tokio::test(flavor = "multi_thread")]
+    async fn select_optional_test() -> anyhow::Result<()> {
+        let pool = SqlitePoolOptions::new().connect(":memory:").await?;
+        sqlx::query("CREATE TABLE ids (id INTEGER PRIMARY KEY)")
+            .execute(&pool)
+            .await?;
+
+        let id: (Option<i64>,) = sqlx::query_as("SELECT MAX(id) FROM ids")
+            .fetch_one(&pool)
+            .await?;
+        assert_eq!(id, (None,));
+        let id: Option<(i64,)> = sqlx::query_as("SELECT MAX(id) FROM ids")
+            .fetch_optional(&pool)
+            .await?;
+        assert_eq!(id, Some((0,)));
+        let id: Option<(i64,)> = sqlx::query_as("SELECT id FROM ids")
+            .fetch_optional(&pool)
+            .await?;
+        assert_eq!(id, None);
+
+        sqlx::query("INSERT INTO ids (id) VALUES (1)")
+            .execute(&pool)
+            .await?;
+
+        let id: (Option<i64>,) = sqlx::query_as("SELECT MAX(id) FROM ids")
+            .fetch_one(&pool)
+            .await?;
+        assert_eq!(id, (Some(1),));
+        let id: Option<(i64,)> = sqlx::query_as("SELECT MAX(id) FROM ids")
+            .fetch_optional(&pool)
+            .await?;
+        assert_eq!(id, Some((1,)));
+        let id: Option<(i64,)> = sqlx::query_as("SELECT id FROM ids")
+            .fetch_optional(&pool)
+            .await?;
+        assert_eq!(id, Some((1,)));
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn insert_test() -> anyhow::Result<()> {
+        let pool = SqlitePoolOptions::new().connect(":memory:").await?;
+        sqlx::query("CREATE TABLE ids (id INTEGER PRIMARY KEY)")
+            .execute(&pool)
+            .await?;
+
+        sqlx::query("INSERT INTO ids (id) VALUES (1)")
+            .execute(&pool)
+            .await?;
+
+        let row: (i64,) = sqlx::query_as("SELECT id FROM ids")
+            .fetch_one(&pool)
+            .await?;
+
+        assert_eq!(row.0, 1);
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn insert_bind_test() -> anyhow::Result<()> {
+        let pool = SqlitePoolOptions::new().connect(":memory:").await?;
+        sqlx::query("CREATE TABLE ids (id INTEGER PRIMARY KEY)")
+            .execute(&pool)
+            .await?;
+
+        sqlx::query("INSERT INTO ids (id) VALUES (?)")
+            .bind(2)
+            .execute(&pool)
+            .await?;
+
+        let row: (i64,) = sqlx::query_as("SELECT id FROM ids")
+            .fetch_one(&pool)
+            .await?;
+
+        assert_eq!(row.0, 2);
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn autoincrement_test() -> anyhow::Result<()> {
+        let pool = SqlitePoolOptions::new().connect(":memory:").await?;
+        sqlx::query("CREATE TABLE ids (id INTEGER PRIMARY KEY AUTOINCREMENT, x TEXT)")
+            .execute(&pool)
+            .await?;
+
+        let rowid1 = sqlx::query("INSERT INTO ids (x) VALUES ('a')")
+            .execute(&pool)
+            .await?
+            .last_insert_rowid();
+
+        let rowid2 = sqlx::query("INSERT INTO ids (x) VALUES ('b')")
+            .execute(&pool)
+            .await?
+            .last_insert_rowid();
+
+        assert_eq!(rowid1, 1);
+        assert_eq!(rowid2, 2);
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
     async fn test() -> anyhow::Result<()> {
         let path_buf: PathBuf = "test.sqlite".parse()?;
         if path_buf.exists() {
