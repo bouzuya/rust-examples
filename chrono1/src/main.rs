6f43c9b337a855f26e67e511d45118928b8ed6d0
bouzuya <m@bouzuya.net>
2021-07-22 16:38:42 +0900

    chrono1: add timestamp conversion test

--- chrono1/src/main.rs
+++ chrono1/src/main.rs
@@ -46,4 +46,23 @@ mod tests {
         }
         Ok(())
     }
+
+    #[test]
+    fn timestamp_convesion_test() -> anyhow::Result<()> {
+        let timestamp = 1612336638;
+        let utc = NaiveDateTime::from_timestamp(timestamp, 0);
+        let dt_utc: DateTime<Utc> = Utc.from_utc_datetime(&utc);
+        assert_eq!(dt_utc.to_rfc3339(), "2021-02-03T07:17:18+00:00");
+        assert_eq!(dt_utc.timestamp(), timestamp);
+        let dt_fixed1: DateTime<FixedOffset> =
+            dt_utc.with_timezone(&FixedOffset::east(9 * 60 * 60));
+        assert_eq!(dt_fixed1.to_rfc3339(), "2021-02-03T16:17:18+09:00");
+        assert_eq!(dt_fixed1.timestamp(), timestamp);
+        assert_eq!(dt_fixed1.with_timezone(&Utc), dt_utc);
+        let dt_fixed2 = FixedOffset::east(9 * 60 * 60).from_utc_datetime(&utc);
+        assert_eq!(dt_fixed2.to_rfc3339(), "2021-02-03T16:17:18+09:00");
+        assert_eq!(dt_fixed2.timestamp(), timestamp);
+        assert_eq!(dt_fixed1, dt_fixed2);
+        Ok(())
+    }
 }
