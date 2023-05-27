a26daf911f60337da66ca49fe8ae258356c0d587
bouzuya <m@bouzuya.net>
2023-05-28 08:30:42 +0900

    axum1: Merge routing example

--- axum1/src/main.rs
+++ axum1/src/main.rs
@@ -1,8 +1,19 @@
 // <https://docs.rs/axum/0.6.18/axum/index.html#example>
+// <https://docs.rs/axum/0.6.18/axum/index.html#routing>
 use axum::{routing::get, Router};
 
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+async fn get_foo() {}
+async fn post_foo() {}
+async fn foo_bar() {}
+
 fn build_app() -> Router {
-    Router::new().route("/", get(|| async { "Hello, World!" }))
+    Router::new()
+        .route("/", get(root))
+        .route("/foo", get(get_foo).post(post_foo))
+        .route("/foo/bar", get(foo_bar))
 }
 
 #[tokio::main]
