d436fb773f7c0049aeeee47781e99d7278fc72ad
bouzuya <m@bouzuya.net>
2023-12-13 21:33:38 +0900

    Add printpdf1

---
+++ printpdf1/src/main.rs
@@ -0,0 +1,14 @@
+use printpdf::*;
+use std::fs::File;
+use std::io::BufWriter;
+
+fn main() -> anyhow::Result<()> {
+    let (doc, _page1, _layer1) =
+        PdfDocument::new("PDF_Document_title", Mm(247.0), Mm(210.0), "Layer 1");
+    let (_page2, _layer1) = doc.add_page(Mm(10.0), Mm(250.0), "Page 2, Layer 1");
+
+    doc.save(&mut BufWriter::new(
+        File::create("test_working.pdf").unwrap(),
+    ))?;
+    Ok(())
+}
