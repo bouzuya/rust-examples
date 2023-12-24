744a91628d309c19bf84fab53d1e80a180508ae5
bouzuya <m@bouzuya.net>
2023-12-24 15:49:01 +0900

    cloud-storage1: Add examples

--- cloud-storage1/src/main.rs
+++ cloud-storage1/src/main.rs
@@ -4,11 +4,14 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use cloud_storage::{Client, NewBucket};
 
+    #[ignore]
     #[tokio::test]
     async fn test() -> anyhow::Result<()> {
-        let bucket_name = "bouzuya-cloud-storage-test";
+        let bucket_name = "xxxxx";
 
         let client = Client::default();
 
@@ -28,9 +31,100 @@ mod tests {
         let buckets = client.bucket().list().await?;
         assert!(!buckets.is_empty());
 
+        {
+            let object_name = "folder/filename.txt";
+
+            match client.object().read(bucket_name, object_name).await {
+                Ok(_) => unreachable!(),
+                Err(e) => match e {
+                    cloud_storage::Error::Google(e) => {
+                        assert_eq!(e.error.code, 404);
+                    }
+                    _ => unreachable!(),
+                },
+            }
+
+            let content = b"Your file is now on google cloud storage!".to_vec();
+            let content_type = "plain/text";
+
+            // <https://cloud.google.com/storage/docs/json_api/v1/objects/insert>
+            client
+                .object()
+                .create(bucket_name, content, object_name, content_type)
+                .await?;
+
+            let mut object = client.object().read(bucket_name, object_name).await?;
+            assert!(object.metadata.is_none());
+            // println!("{:#?}", object);
+
+            let uuid = uuid::Uuid::new_v4().to_string();
+            object.metadata = Some({
+                let mut metadata = HashMap::new();
+                metadata.insert("firebaseStorageDownloadTokens".to_string(), uuid);
+                metadata
+            });
+            client.object().update(&object).await?;
+
+            let object = client.object().read(bucket_name, object_name).await?;
+            assert!(object.metadata.as_ref().is_some_and(|m| m.len() == 1));
+            // println!("{:#?}", object);
+
+            // <https://cloud.google.com/storage/docs/json_api/v1/objects/delete>
+            client.object().delete(bucket_name, object_name).await?;
+        }
+
         // <https://cloud.google.com/storage/docs/json_api/v1/buckets/delete>
         client.bucket().delete(created_bucket).await?;
 
+        Ok(())
+    }
+
+    #[ignore]
+    #[tokio::test]
+    async fn test2() -> anyhow::Result<()> {
+        let bucket_name = "xxxxx.appspot.com";
+
+        let client = Client::default();
+
+        let object_name = "a.txt";
+
+        match client.object().read(bucket_name, object_name).await {
+            Ok(_) => unreachable!(),
+            Err(e) => match e {
+                cloud_storage::Error::Google(e) => {
+                    assert_eq!(e.error.code, 404);
+                }
+                _ => unreachable!(),
+            },
+        }
+
+        let content = b"Your file is now on google cloud storage!".to_vec();
+        let content_type = "plain/text";
+
+        // <https://cloud.google.com/storage/docs/json_api/v1/objects/insert>
+        client
+            .object()
+            .create(bucket_name, content, object_name, content_type)
+            .await?;
+
+        let mut object = client.object().read(bucket_name, object_name).await?;
+        assert!(object.metadata.is_none());
+        // println!("{:#?}", object);
+
+        let uuid = uuid::Uuid::new_v4().to_string();
+        object.metadata = Some({
+            let mut metadata = HashMap::new();
+            metadata.insert("firebaseStorageDownloadTokens".to_string(), uuid);
+            metadata
+        });
+        client.object().update(&object).await?;
+
+        let object = client.object().read(bucket_name, object_name).await?;
+        assert!(object.metadata.as_ref().is_some_and(|m| m.len() == 1));
+        // println!("{:#?}", object);
+
+        // <https://cloud.google.com/storage/docs/json_api/v1/objects/delete>
+        client.object().delete(bucket_name, object_name).await?;
         Ok(())
     }
 }
