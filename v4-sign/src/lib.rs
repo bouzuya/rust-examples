9c7e1c3e57f730fc2fa6783ba7f965dd24eb7419
bouzuya <m@bouzuya.net>
2024-03-25 23:36:59 +0900

    Add test_chrono

--- v4-sign/src/lib.rs
+++ v4-sign/src/lib.rs
@@ -38,4 +38,17 @@ mod tests {
             "%3F%3D%21%23%24%26%27%28%29%2A%2B%2C%3A%3B%40%5B%5D%22"
         );
     }
+
+    #[test]
+    fn test_chrono() -> anyhow::Result<()> {
+        let date_time =
+            chrono::DateTime::<chrono::FixedOffset>::parse_from_rfc3339("2020-01-02T03:04:05Z")?
+                .naive_utc()
+                .and_utc();
+        let date = date_time.format("%Y%m%d").to_string();
+        let x_goog_date = date_time.format("%Y%m%dT%H%M%SZ").to_string();
+        assert_eq!(date, "20200102");
+        assert_eq!(x_goog_date, "20200102T030405Z");
+        Ok(())
+    }
 }
