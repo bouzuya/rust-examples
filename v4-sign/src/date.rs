0b725cbe43690d12091bd0209109939714fee850
bouzuya <m@bouzuya.net>
2024-03-30 20:07:16 +0900

    v4-sign: Add Date::from_unix_timestamp

--- v4-sign/src/date.rs
+++ v4-sign/src/date.rs
@@ -1,23 +1,41 @@
-use chrono::Datelike;
+#[derive(Debug, thiserror::Error)]
+#[error(transparent)]
+pub(crate) struct Error(#[from] ErrorKind);
 
 #[derive(Debug, thiserror::Error)]
-#[error("year is out of range (0..=9999) : {0}")]
-pub(crate) struct Error(i64);
+enum ErrorKind {
+    #[error("timestamp is out of range : {0}")]
+    TimestampOutOfRange(i64),
+    #[error("year is out of range (0..=9999) : {0}")]
+    YearOutOfRange(i64),
+}
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub(crate) struct Date(u32);
+
+impl Date {
+    pub(crate) fn from_unix_timestamp(unix_timestamp: i64) -> Result<Self, Error> {
+        // 0000-01-01T00:00:00Z..=9999-12-31T23:59:59Z
+        if !(-62_167_219_200..=253_402_300_799).contains(&unix_timestamp) {
+            return Err(Error::from(ErrorKind::TimestampOutOfRange(unix_timestamp)));
+        }
+        let chrono_date_time = chrono::DateTime::from_timestamp(unix_timestamp, 0_u32)
+            .ok_or(ErrorKind::TimestampOutOfRange(unix_timestamp))?;
+        Self::try_from(chrono_date_time)
+    }
+}
 
 impl std::convert::TryFrom<chrono::DateTime<chrono::Utc>> for Date {
     type Error = Error;
 
     fn try_from(value: chrono::DateTime<chrono::Utc>) -> Result<Self, Self::Error> {
-        let year = value.year();
+        let year = chrono::Datelike::year(&value);
         if !(0..=9999).contains(&year) {
-            return Err(Error(i64::from(year)));
+            return Err(Error::from(ErrorKind::YearOutOfRange(i64::from(year))));
         }
         let year = year as u32;
-        let month = value.month();
-        let day = value.day();
+        let month = chrono::Datelike::month(&value);
+        let day = chrono::Datelike::day(&value);
         let value = year * 10000 + month * 100 + day;
         Ok(Self(value))
     }
@@ -52,6 +70,21 @@ mod tests {
         assert_eq!(chrono_date_time.to_rfc3339(), "+12000-01-02T03:04:05+00:00");
         let date = Date::try_from(chrono_date_time);
         assert!(date.is_err());
+
+        assert!(Date::from_unix_timestamp(-62_167_219_201).is_err());
+        assert_eq!(
+            Date::from_unix_timestamp(-62_167_219_200)?.to_string(),
+            "00000101"
+        );
+        assert_eq!(Date::from_unix_timestamp(-1)?.to_string(), "19691231");
+        assert_eq!(Date::from_unix_timestamp(0)?.to_string(), "19700101");
+        assert_eq!(Date::from_unix_timestamp(86399)?.to_string(), "19700101");
+        assert_eq!(Date::from_unix_timestamp(86400)?.to_string(), "19700102");
+        assert_eq!(
+            Date::from_unix_timestamp(253_402_300_799)?.to_string(),
+            "99991231"
+        );
+        assert!(Date::from_unix_timestamp(253_402_300_800).is_err());
         Ok(())
     }
 }
