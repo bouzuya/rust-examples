4238affaa1d7407a18ac05514806fee7ff84c586
bouzuya <m@bouzuya.net>
2024-04-01 22:44:18 +0900

    v4-sign: Add impl Serialize for PolicyDocument

--- v4-sign/src/policy_document.rs
+++ v4-sign/src/policy_document.rs
@@ -9,7 +9,7 @@ pub use self::field::Field;
 pub use self::value::Value;
 
 // <https://cloud.google.com/storage/docs/authentication/signatures#policy-document>
-#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct PolicyDocument {
     pub conditions: Vec<Condition>,
     pub expiration: Expiration,
@@ -24,10 +24,25 @@ mod tests {
     #[test]
     fn test() -> anyhow::Result<()> {
         fn assert_impls<
-            T: Clone + std::fmt::Debug + Eq + PartialEq + serde::Deserialize<'static>,
+            T: Clone
+                + std::fmt::Debug
+                + Eq
+                + PartialEq
+                + serde::Deserialize<'static>
+                + serde::Serialize,
         >() {
         }
         assert_impls::<PolicyDocument>();
+
+        let policy_document = PolicyDocument {
+            conditions: vec![
+                Condition::ExactMatching(Field::new("bucket")?, Value::new("travel-maps")),
+                Condition::ExactMatching(Field::new("Content-Type")?, Value::new("image/jpeg")),
+                Condition::StartsWith(Field::new("key")?, Value::new("")),
+                Condition::ContentLengthRange(0, 1000000),
+            ],
+            expiration: Expiration::from_str("20200616T111111Z")?,
+        };
 
         let json = r#"
 {
@@ -43,16 +58,40 @@ mod tests {
         .trim();
         assert_eq!(
             serde_json::from_str::<PolicyDocument>(json)?,
-            PolicyDocument {
-                conditions: vec![
-                    Condition::ExactMatching(Field::new("bucket")?, Value::new("travel-maps")),
-                    Condition::ExactMatching(Field::new("Content-Type")?, Value::new("image/jpeg")),
-                    Condition::StartsWith(Field::new("key")?, Value::new("")),
-                    Condition::ContentLengthRange(0, 1000000),
-                ],
-                expiration: Expiration::from_str("20200616T111111Z")?,
-            }
+            policy_document
         );
+        assert_eq!(
+            serde_json::to_string_pretty(&policy_document)?,
+            r#"
+{
+  "conditions": [
+    [
+      "eq",
+      "$bucket",
+      "travel-maps"
+    ],
+    [
+      "eq",
+      "$Content-Type",
+      "image/jpeg"
+    ],
+    [
+      "starts-with",
+      "$key",
+      ""
+    ],
+    [
+      "content-length-range",
+      0,
+      1000000
+    ]
+  ],
+  "expiration": "20200616T111111Z"
+}
+            "#
+            .trim()
+        );
+
         Ok(())
     }
 }
--- v4-sign/tests/policy_document.rs
+++ v4-sign/tests/policy_document.rs
@@ -49,12 +49,103 @@ fn test_policy_document() -> anyhow::Result<()> {
                         Value::new("example_account@example_project.iam.gserviceaccount.com/20191102/us-central1/storage/goog4_request")
                     ),
                     Condition::ExactMatching(
-                      Field::new("x-goog-date")?,
-                      Value::new("20191102T043530Z")
+                        Field::new("x-goog-date")?,
+                        Value::new("20191102T043530Z")
                     )
                 ],
                 expiration: Expiration::from_str("20200616T111111Z")?,
             }
         );
+    Ok(())
+}
+
+#[test]
+fn test_impl_serialize_policy_document() -> anyhow::Result<()> {
+    use std::str::FromStr as _;
+    use v4_sign::policy_document::{Condition, Expiration, Field, PolicyDocument, Value};
+
+    let policy_document = PolicyDocument {
+                conditions: vec![
+                    Condition::StartsWith(
+                        Field::new("key")?,
+                        Value::new("")
+                    ),
+                    Condition::ExactMatching(
+                        Field::new("bucket")?,
+                        Value::new("travel-maps")
+                    ),
+                    Condition::ExactMatching(
+                        Field::new("success_action_redirect")?,
+                        Value::new("http://www.example.com/success_notification.html")
+                    ),
+                    Condition::ExactMatching(
+                        Field::new("Content-Type")?,
+                        Value::new("image/jpeg")
+                    ),
+                    Condition::ContentLengthRange(0, 1000000),
+                    Condition::ExactMatching(
+                        Field::new("x-goog-algorithm")?,
+                        Value::new("GOOG4-RSA-SHA256")
+                    ),
+                    Condition::ExactMatching(
+                        Field::new("x-goog-credential")?,
+                        Value::new("example_account@example_project.iam.gserviceaccount.com/20191102/us-central1/storage/goog4_request")
+                    ),
+                    Condition::ExactMatching(
+                        Field::new("x-goog-date")?,
+                        Value::new("20191102T043530Z")
+                    )
+                ],
+                expiration: Expiration::from_str("20200616T111111Z")?,
+            };
+    assert_eq!(
+            serde_json::to_string_pretty(&policy_document)?,
+r#"
+{
+  "conditions": [
+    [
+      "starts-with",
+      "$key",
+      ""
+    ],
+    [
+      "eq",
+      "$bucket",
+      "travel-maps"
+    ],
+    [
+      "eq",
+      "$success_action_redirect",
+      "http://www.example.com/success_notification.html"
+    ],
+    [
+      "eq",
+      "$Content-Type",
+      "image/jpeg"
+    ],
+    [
+      "content-length-range",
+      0,
+      1000000
+    ],
+    [
+      "eq",
+      "$x-goog-algorithm",
+      "GOOG4-RSA-SHA256"
+    ],
+    [
+      "eq",
+      "$x-goog-credential",
+      "example_account@example_project.iam.gserviceaccount.com/20191102/us-central1/storage/goog4_request"
+    ],
+    [
+      "eq",
+      "$x-goog-date",
+      "20191102T043530Z"
+    ]
+  ],
+  "expiration": "20200616T111111Z"
+}
+"#.trim());
     Ok(())
 }
