77559e1d760f6f6ad6d0b7182c260fbdd0220131
bouzuya <m@bouzuya.net>
2025-02-03 21:22:34 +0900

    tonic-server1: Add ScalarValueType rpc

--- tonic-server1/src/main.rs
+++ tonic-server1/src/main.rs
@@ -1,5 +1,3 @@
-use hello::greeter_server::{Greeter, GreeterServer};
-
 pub mod hello {
     tonic::include_proto!("hello");
 }
@@ -7,7 +5,7 @@ pub mod hello {
 pub struct MyGreeter;
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
+impl hello::greeter_server::Greeter for MyGreeter {
     async fn hello(
         &self,
         request: tonic::Request<hello::HelloRequest>,
@@ -19,12 +17,56 @@ impl Greeter for MyGreeter {
     }
 }
 
+pub struct MyService2;
+
+#[tonic::async_trait]
+impl hello::service2_server::Service2 for MyService2 {
+    async fn scalar_value_type(
+        &self,
+        request: tonic::Request<hello::ScalarValueTypeRequest>,
+    ) -> Result<tonic::Response<hello::ScalarValueTypeResponse>, tonic::Status> {
+        let hello::ScalarValueTypeRequest {
+            double_value,
+            float_value,
+            int32_value,
+            int64_value,
+            uint32_value,
+            uint64_value,
+            sint32_value,
+            sint64_value,
+            fixed32_value,
+            fixed64_value,
+            sfixed32_value,
+            sfixed64_value,
+            bool_value,
+            string_value,
+            bytes_value,
+        } = request.into_inner();
+        let response = hello::ScalarValueTypeResponse {
+            double_value,
+            float_value,
+            int32_value,
+            int64_value,
+            uint32_value,
+            uint64_value,
+            sint32_value,
+            sint64_value,
+            fixed32_value,
+            fixed64_value,
+            sfixed32_value,
+            sfixed64_value,
+            bool_value,
+            string_value,
+            bytes_value,
+        };
+        Ok(tonic::Response::new(response))
+    }
+}
 #[tokio::main]
 async fn main() {
-    let greeter = MyGreeter;
-
     tonic::transport::Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(hello::greeter_server::GreeterServer::new(MyGreeter))
+        .add_service(hello::service2_server::Service2Server::new(MyService2))
         .serve("0.0.0.0:3000".parse().unwrap())
         .await
         .unwrap();
