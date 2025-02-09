fbac3814b8873768c7b7e1104f734855d111f336
bouzuya <m@bouzuya.net>
2025-02-09 23:02:37 +0900

    lopdf1: Add unit mod

--- lopdf1/src/main.rs
+++ lopdf1/src/main.rs
@@ -1,8 +1,10 @@
 mod bits_per_component;
 mod color_space;
 mod image;
+mod unit;
 
 use image::Image;
+use unit::F32Ext as _;
 
 fn main() {
     let mut document = lopdf::Document::load("./a-output.pdf").unwrap();
@@ -23,47 +25,20 @@ fn main() {
     let page_id = *document.get_pages().get(&1_u32).unwrap();
     // let image = Image::from_png_file_path("./bouzuya.png").unwrap();
     let image = Image::from_png_file_path("./dummy2.png").unwrap();
-    let size = (image.width() as f32, image.height() as f32);
+    let width = (image.width() as f32).px();
+    let height = (image.height() as f32).px();
     let stream = image.into_lopdf_stream();
-    let x = mm_to_px(210.0 - px_to_mm(size.0));
-    let y = mm_to_px(297.0 - px_to_mm(size.1));
+    let x = 210.0.mm().to_px() - width;
+    let y = 297.0.mm().to_px() - height;
     document
-        .insert_image(page_id, stream, (x, y), size)
+        .insert_image(
+            page_id,
+            stream,
+            (x.to_f32(), y.to_f32()),
+            (width.to_f32(), height.to_f32()),
+        )
         .unwrap();
     let file = std::fs::File::create_new("o.pdf").unwrap();
     let mut writer = std::io::BufWriter::new(file);
     document.save_to(&mut writer).unwrap();
-}
-
-fn mm_to_px(mm: f32) -> f32 {
-    let mmpi = 25.4; // mm per inch
-    let dpi = 72.0; // dot (px) per inch
-    let px = mm / mmpi * dpi;
-    px
-}
-
-fn px_to_mm(px: f32) -> f32 {
-    let mmpi = 25.4; // mm per inch
-    let dpi = 72.0; // dot (px) per inch
-    let mm = px / dpi * mmpi;
-    mm
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_mm_to_px() {
-        // A4 = 210mm x 297mm
-        assert_eq!(mm_to_px(210.0), 595.2756);
-        assert_eq!(mm_to_px(297.0), 841.88983);
-    }
-
-    #[test]
-    fn test_px_to_mm() {
-        // A4 = 210mm x 297mm
-        assert_eq!(px_to_mm(595.2756), 210.0);
-        assert_eq!(px_to_mm(841.88983), 297.0);
-    }
 }
