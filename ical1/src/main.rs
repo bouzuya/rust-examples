f022036b03ea6209b2ebf0b8cc14fd384588fe65
bouzuya <m@bouzuya.net>
2025-03-14 07:45:01 +0900

    ical1: Add unfold

--- ical1/src/main.rs
+++ ical1/src/main.rs
@@ -73,6 +73,45 @@ where
     lines
 }
 
+fn unfold<I>(mut iter: I) -> String
+where
+    I: Iterator<Item = char>,
+{
+    let mut lines = String::new();
+    let mut crlf = false;
+    while let Some(c) = iter.next() {
+        if crlf {
+            crlf = false;
+            if c == ' ' || c == '\t' {
+                // Unfolding
+            } else {
+                lines.push('\r');
+                lines.push('\n');
+                lines.push(c);
+            }
+        } else {
+            if c == '\r' {
+                match iter.next() {
+                    Some('\n') => {
+                        // CRLF
+                        crlf = true;
+                    }
+                    _ => {
+                        // CR without LF
+                        unreachable!();
+                    }
+                }
+            } else {
+                lines.push(c);
+            }
+        }
+    }
+    assert!(crlf);
+    lines.push('\r');
+    lines.push('\n');
+    lines
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,6 +133,33 @@ mod tests {
                 " 67890\r\n"
             ]
             .join("")
+        );
+    }
+
+    #[test]
+    fn test_unfold() {
+        assert_eq!(
+            unfold(
+                "DESCRIPTION:This is a long description that exists on a long line.\r\n".chars()
+            ),
+            "DESCRIPTION:This is a long description that exists on a long line.\r\n"
+        );
+        assert_eq!(
+            unfold(
+                "DESCRIPTION:This is a lo\r\n ng description\r\n  that exists on a long line.\r\n"
+                    .chars()
+            ),
+            "DESCRIPTION:This is a long description that exists on a long line.\r\n"
+        );
+
+        assert_eq!(
+            unfold(
+                [
+                    "1234567890:2345678901234567890123456789012345678901234567890123456789012345\r\n",
+                    " 67890\r\n"
+                ].join("").chars()
+            ),
+            "1234567890:234567890123456789012345678901234567890123456789012345678901234567890\r\n"
         );
     }
 }
