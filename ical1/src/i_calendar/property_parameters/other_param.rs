a5277ee426092f33101d8642e217003b48f7856f
bouzuya <m@bouzuya.net>
2025-03-18 22:58:37 +0900

    ical1: Add OtherParam::to_escaped

--- ical1/src/i_calendar/property_parameters/other_param.rs
+++ ical1/src/i_calendar/property_parameters/other_param.rs
@@ -1,7 +1,28 @@
 use crate::i_calendar::property_parameters::{IanaParam, XParam};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub(in crate::i_calendar) enum OtherParam {
+pub enum OtherParam {
     IanaParam(IanaParam),
     XParam(XParam),
+}
+
+impl OtherParam {
+    pub(in crate::i_calendar) fn to_escaped(&self) -> String {
+        match self {
+            Self::IanaParam(p) => p.to_escaped(),
+            Self::XParam(p) => p.to_escaped(),
+        }
+    }
+}
+
+impl From<IanaParam> for OtherParam {
+    fn from(p: IanaParam) -> Self {
+        Self::IanaParam(p)
+    }
+}
+
+impl From<XParam> for OtherParam {
+    fn from(value: XParam) -> Self {
+        Self::XParam(value)
+    }
 }
