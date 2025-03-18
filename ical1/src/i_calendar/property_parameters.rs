d13b0ef9ea1d6a9f7e3bea968710977d9c7034ea
bouzuya <m@bouzuya.net>
2025-03-18 21:23:56 +0900

    ical1: Add IanaToken

--- ical1/src/i_calendar/property_parameters.rs
+++ ical1/src/i_calendar/property_parameters.rs
@@ -1,4 +1,7 @@
+mod iana_token;
 mod param_value;
 
+pub use self::iana_token::IanaToken;
+pub use self::iana_token::IanaTokenError;
 pub use self::param_value::ParamValue;
 pub use self::param_value::ParamValueError;
---
+++ ical1/src/i_calendar/property_parameters/iana_token.rs
@@ -0,0 +1,48 @@
+#[derive(Debug, thiserror::Error)]
+#[error("iana-token")]
+pub struct IanaTokenError {
+    _private: (),
+}
+
+// iana-token    = 1*(ALPHA / DIGIT / "-")
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
+pub struct IanaToken(String);
+
+impl IanaToken {
+    fn from_unescaped(s: &str) -> Result<Self, IanaTokenError> {
+        if s.is_empty() || !s.chars().all(|c| c.is_ascii_alphanumeric() || c == '-') {
+            Err(IanaTokenError { _private: () })
+        } else {
+            Ok(Self(s.to_owned()))
+        }
+    }
+
+    fn to_unescaped(&self) -> String {
+        self.0.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() -> anyhow::Result<()> {
+        fn assert_fn<T: Clone + Eq + Ord + PartialEq + PartialOrd>() {}
+        assert_fn::<IanaToken>();
+
+        let s = "IANA-TOKEN";
+        assert_eq!(IanaToken::from_unescaped(s)?.to_unescaped(), s);
+
+        let s = "iana-token-1234";
+        assert_eq!(IanaToken::from_unescaped(s)?.to_unescaped(), s);
+
+        let s = "";
+        assert!(IanaToken::from_unescaped(s).is_err());
+
+        let s = "IANA_TOKEN";
+        assert!(IanaToken::from_unescaped(s).is_err());
+
+        Ok(())
+    }
+}
