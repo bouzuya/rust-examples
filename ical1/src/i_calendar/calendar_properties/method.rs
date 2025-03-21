d710f32a0c941e6a436ad6b46cd5db5e0273ed47
bouzuya <m@bouzuya.net>
2025-03-21 22:20:53 +0900

    ical1: Remove Method::from_string

--- ical1/src/i_calendar/calendar_properties/method.rs
+++ ical1/src/i_calendar/calendar_properties/method.rs
@@ -17,7 +17,6 @@ enum ErrorInner {
     InvalidFormat,
 }
 
-/// metparam not supported
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Method(Text, Vec<OtherParam>);
 
@@ -40,21 +39,7 @@ impl Method {
         )
     }
 
-    pub(in crate::i_calendar) fn from_string(s: String) -> Result<Self, MethodError> {
-        if s.starts_with("METHOD:") && s.ends_with("\r\n") {
-            Ok(Text::from_string(
-                s.trim_start_matches("METHOD:")
-                    .trim_end_matches("\r\n")
-                    .to_owned(),
-            )
-            .map(|text| Self(text, Vec::new()))
-            .map_err(ErrorInner::Text)?)
-        } else {
-            Err(ErrorInner::InvalidFormat)?
-        }
-    }
-
-    pub(in crate::i_calendar) fn into_string(self) -> String {
+    pub(in crate::i_calendar) fn to_escaped(&self) -> String {
         let mut s = String::new();
         s.push_str("METHOD");
         for p in &self.1 {
@@ -70,7 +55,7 @@ impl Method {
 
 #[cfg(test)]
 mod tests {
-    use crate::i_calendar::property_parameters::{IanaParam, IanaToken, ParamValue};
+    use crate::i_calendar::property_parameters::{ParamValue, XName, XParam};
 
     use super::*;
 
@@ -79,28 +64,22 @@ mod tests {
         fn assert_fn<T: Clone + Eq + PartialEq>() {}
         assert_fn::<Method>();
 
-        let s = "METHOD:REQUEST\r\n".to_owned();
-        assert_eq!(Method::from_string(s.clone())?.into_string(), s);
-
-        let s = "METHOD:REQUEST".to_owned();
-        assert!(Method::from_string(s).is_err());
-
         let s = "REQUEST";
         assert_eq!(
-            Method::new(Text::from_unescaped(s)?)?.into_string(),
+            Method::new(Text::from_unescaped(s)?)?.to_escaped(),
             "METHOD:REQUEST\r\n"
         );
 
         assert_eq!(
             Method::with_parameters(
                 Text::from_unescaped("REQUEST")?,
-                [IanaParam::new(
-                    IanaToken::from_unescaped("PARAMETER")?,
+                [XParam::new(
+                    XName::from_unescaped("X-PARAM")?,
                     vec![ParamValue::from_unescaped("value")?]
                 )?]
             )
-            .into_string(),
-            "METHOD;PARAMETER=value:REQUEST\r\n"
+            .to_escaped(),
+            "METHOD;X-PARAM=value:REQUEST\r\n"
         );
 
         Ok(())
