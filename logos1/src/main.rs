58549bcd43578e391da251bcd8b97c691ecd637a
bouzuya <m@bouzuya.net>
2025-04-24 23:32:16 +0900

    logos1: Add token4

--- logos1/src/main.rs
+++ logos1/src/main.rs
@@ -43,7 +43,6 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-
     #[test]
     fn test_readme_example() {
         #[derive(logos::Logos, Debug, PartialEq)]
@@ -131,6 +130,58 @@ mod tests {
         assert_eq!(lexer.next(), Some(Ok(Token3::Date((2020_u16, 1_u8, 2_u8)))));
         assert_eq!(lexer.span(), 0..10);
         assert_eq!(lexer.slice(), "2020-01-02");
+
+        assert_eq!(lexer.next(), None);
+    }
+
+    #[derive(Debug, PartialEq, logos::Logos)]
+    #[logos(skip r"\s+")]
+    enum Token4 {
+        #[token("false")]
+        False,
+        #[token("true")]
+        True,
+        #[token("?")]
+        Question,
+        #[token(":")]
+        Colon,
+        #[regex("0|[1-9][0-9]{0,}", |lexer| lexer.slice().parse::<u8>().ok())]
+        Integer(u8),
+        #[token("+")]
+        Plus,
+    }
+
+    #[test]
+    fn test_token4() {
+        let mut lexer = <Token4 as logos::Logos>::lexer("true ? 0 : 1 + 2");
+
+        assert_eq!(lexer.next(), Some(Ok(Token4::True)));
+        assert_eq!(lexer.span(), 0..4);
+        assert_eq!(lexer.slice(), "true");
+
+        assert_eq!(lexer.next(), Some(Ok(Token4::Question)));
+        assert_eq!(lexer.span(), 5..6);
+        assert_eq!(lexer.slice(), "?");
+
+        assert_eq!(lexer.next(), Some(Ok(Token4::Integer(0))));
+        assert_eq!(lexer.span(), 7..8);
+        assert_eq!(lexer.slice(), "0");
+
+        assert_eq!(lexer.next(), Some(Ok(Token4::Colon)));
+        assert_eq!(lexer.span(), 9..10);
+        assert_eq!(lexer.slice(), ":");
+
+        assert_eq!(lexer.next(), Some(Ok(Token4::Integer(1))));
+        assert_eq!(lexer.span(), 11..12);
+        assert_eq!(lexer.slice(), "1");
+
+        assert_eq!(lexer.next(), Some(Ok(Token4::Plus)));
+        assert_eq!(lexer.span(), 13..14);
+        assert_eq!(lexer.slice(), "+");
+
+        assert_eq!(lexer.next(), Some(Ok(Token4::Integer(2))));
+        assert_eq!(lexer.span(), 15..16);
+        assert_eq!(lexer.slice(), "2");
 
         assert_eq!(lexer.next(), None);
     }
