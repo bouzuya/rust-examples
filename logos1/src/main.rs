3718f7d36de1bf2efd8a91ec9f1423ef1f635e18
bouzuya <m@bouzuya.net>
2025-04-25 22:15:36 +0900

    logos1: Add term

--- logos1/src/main.rs
+++ logos1/src/main.rs
@@ -185,4 +185,126 @@ mod tests {
 
         assert_eq!(lexer.next(), None);
     }
+
+    #[test]
+    fn test_token4_with_expr() {
+        // unary = false | true | integer
+        // binary = unary | unary plus binary
+        // ternary = binary | binary question ternary colon ternary
+
+        #[derive(Debug, PartialEq)]
+        enum Term {
+            False,
+            True,
+            Integer(u8),
+            Add(Box<Term>, Box<Term>),
+            If(Box<Term>, Box<Term>, Box<Term>),
+        }
+
+        fn read_binary(iter: &mut std::iter::Peekable<logos::Lexer<'_, Token4>>) -> Term {
+            let token = iter.next().unwrap().unwrap();
+            let unary = match token {
+                Token4::False => Term::False,
+                Token4::True => Term::True,
+                Token4::Integer(i) => Term::Integer(i),
+                Token4::Plus | Token4::Question | Token4::Colon => unreachable!(),
+            };
+            match iter.peek() {
+                None => unary,
+                Some(token) => {
+                    let token = token.as_ref().unwrap();
+                    match token {
+                        Token4::False | Token4::True | Token4::Integer(_) => unreachable!(),
+                        Token4::Question | Token4::Colon => unary,
+                        Token4::Plus => {
+                            assert!(matches!(iter.next(), Some(Ok(Token4::Plus))));
+                            let left = unary;
+                            let right = read_binary(iter);
+                            Term::Add(Box::new(left), Box::new(right))
+                        }
+                    }
+                }
+            }
+        }
+
+        fn read_ternary(iter: &mut std::iter::Peekable<logos::Lexer<'_, Token4>>) -> Term {
+            let binary = read_binary(iter);
+            match iter.peek() {
+                None => binary,
+                Some(token) => {
+                    let token = token.as_ref().unwrap();
+                    match token {
+                        Token4::False | Token4::True | Token4::Integer(_) | Token4::Plus => {
+                            unreachable!()
+                        }
+                        Token4::Question => {
+                            assert!(matches!(iter.next(), Some(Ok(Token4::Question))));
+                            let cond = binary;
+                            let thn = read_ternary(iter);
+                            assert!(matches!(iter.next(), Some(Ok(Token4::Colon))));
+                            let els = read_ternary(iter);
+                            Term::If(Box::new(cond), Box::new(thn), Box::new(els))
+                        }
+                        Token4::Colon => binary,
+                    }
+                }
+            }
+        }
+
+        fn test_parse(s: &str, expected: Term) {
+            let lexer = <Token4 as logos::Logos>::lexer(s);
+            let term = read_ternary(&mut lexer.peekable());
+            assert_eq!(term, expected);
+        }
+
+        test_parse("true", Term::True);
+        test_parse("false", Term::False);
+        test_parse("0", Term::Integer(0));
+        test_parse(
+            "1 + 2",
+            Term::Add(Box::new(Term::Integer(1)), Box::new(Term::Integer(2))),
+        );
+        test_parse(
+            "3 + 4 + 5",
+            Term::Add(
+                Box::new(Term::Integer(3)),
+                Box::new(Term::Add(
+                    Box::new(Term::Integer(4)),
+                    Box::new(Term::Integer(5)),
+                )),
+            ),
+        );
+        test_parse(
+            "true ? 6 : 7",
+            Term::If(
+                Box::new(Term::True),
+                Box::new(Term::Integer(6)),
+                Box::new(Term::Integer(7)),
+            ),
+        );
+        test_parse(
+            "true ? true ? 8 : 9 : 10",
+            Term::If(
+                Box::new(Term::True),
+                Box::new(Term::If(
+                    Box::new(Term::True),
+                    Box::new(Term::Integer(8)),
+                    Box::new(Term::Integer(9)),
+                )),
+                Box::new(Term::Integer(10)),
+            ),
+        );
+        test_parse(
+            "true ? 11 : true ? 12 : 13",
+            Term::If(
+                Box::new(Term::True),
+                Box::new(Term::Integer(11)),
+                Box::new(Term::If(
+                    Box::new(Term::True),
+                    Box::new(Term::Integer(12)),
+                    Box::new(Term::Integer(13)),
+                )),
+            ),
+        );
+    }
 }
