61beba5ca2a8ed57ac910abcd12a9a2b765b5fd8
bouzuya <m@bouzuya.net>
2025-06-01 22:19:25 +0900

    google-cloud-auth-v0-20: Add reqwest example

--- google-cloud-auth-v0-20/src/main.rs
+++ google-cloud-auth-v0-20/src/main.rs
@@ -1,9 +1,22 @@
 #[tokio::main]
 async fn main() {
+    let project_id = std::env::var("PROJECT_ID").unwrap();
+
     let credentials: google_cloud_auth::credentials::Credentials =
         google_cloud_auth::credentials::Builder::default()
             .build()
             .unwrap();
     let header_map: http::HeaderMap = credentials.headers(http::Extensions::new()).await.unwrap();
-    println!("{:#?}", header_map);
+
+    let client: reqwest::Client = reqwest::ClientBuilder::new().build().unwrap();
+    let response: reqwest::Response = client
+        .get(format!(
+            "https://storage.googleapis.com/storage/v1/b?project={}",
+            project_id
+        ))
+        .headers(header_map)
+        .send()
+        .await
+        .unwrap();
+    println!("{}", response.text().await.unwrap());
 }
