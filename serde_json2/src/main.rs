36b2d17c95c3c21c532d57befcba74af5f181274
bouzuya <m@bouzuya.net>
2025-09-20 21:12:29 +0900

    Update serde_json2

--- serde_json2/src/main.rs
+++ serde_json2/src/main.rs
@@ -8,9 +8,10 @@ enum Permission {
 impl std::fmt::Display for Permission {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         serde_json::to_value(&self)
-            .expect("Permission should be serializable")
-            .as_str()
-            .expect("Permission should serialize to a string")
+            .ok()
+            .as_ref()
+            .and_then(serde_json::Value::as_str)
+            .expect("should be a string")
             .fmt(f)
     }
 }
