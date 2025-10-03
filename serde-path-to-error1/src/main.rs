6a4364f498835e3c598da4ebfd406ea09a26abac
bouzuya <m@bouzuya.net>
2025-10-03 22:34:10 +0900

    Update serde-path-to-error1

--- serde-path-to-error1/src/main.rs
+++ serde-path-to-error1/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    #[derive(serde::Deserialize)]
+    #[derive(Debug, serde::Deserialize)]
     struct Package {
         name: String,
         dependencies: std::collections::BTreeMap<String, Dependency>,
     }
 
-    #[derive(serde::Deserialize)]
+    #[derive(Debug, serde::Deserialize)]
     struct Dependency {
         version: String,
     }
@@ -28,4 +28,101 @@ fn main() {
             assert_eq!(path, "dependencies.serde.version");
         }
     }
+
+    let value = serde_firestore_value::google::firestore::v1::Value {
+        value_type: Some(
+            serde_firestore_value::google::firestore::v1::value::ValueType::MapValue(
+                serde_firestore_value::google::firestore::v1::MapValue {
+                    fields: std::collections::HashMap::from_iter([
+                        ("name".to_owned(), serde_firestore_value::google::firestore::v1::Value {
+                            value_type: Some(
+                                serde_firestore_value::google::firestore::v1::value::ValueType::StringValue(
+                                    "demo2".to_string(),
+                                ),
+                            ),
+                        }),
+                        ("dependencies".to_owned(), serde_firestore_value::google::firestore::v1::Value {
+                            value_type: Some(
+                                serde_firestore_value::google::firestore::v1::value::ValueType::MapValue(
+                                    serde_firestore_value::google::firestore::v1::MapValue {
+                                        fields: std::collections::HashMap::from_iter([
+                                            ("serde".to_owned(), serde_firestore_value::google::firestore::v1::Value {
+                                                value_type: Some(
+                                                    serde_firestore_value::google::firestore::v1::value::ValueType::MapValue(
+                                                        serde_firestore_value::google::firestore::v1::MapValue {
+                                                            fields: std::collections::HashMap::from_iter([
+                                                                ("version".to_owned(), serde_firestore_value::google::firestore::v1::Value {
+                                                                    value_type: Some(
+                                                                        // This should be a StringValue, but we use an IntegerValue to trigger a type error
+                                                                        serde_firestore_value::google::firestore::v1::value::ValueType::IntegerValue(1),
+                                                                    ),
+                                                                }),
+                                                            ]),
+                                                        },
+                                                    ),
+                                                ),
+                                            }),
+                                        ]),
+                                    },
+                                ),
+                            ),
+                        }),
+                    ])
+                }
+            ),
+        ),
+    };
+
+    let deserializer = serde_firestore_value::Deserializer::new(&value);
+    let result: Result<Package, _> = serde::de::Deserialize::deserialize(deserializer);
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err().to_string(),
+        "invalid type: integer value, expected string value"
+    );
+
+    let deserializer = serde_firestore_value::Deserializer::new(&value);
+    let result: Result<Package, _> = serde_path_to_error::deserialize(deserializer);
+    match result {
+        Ok(_) => panic!("expected a type error"),
+        Err(err) => {
+            let path = err.path().to_string();
+            assert_eq!(path, "dependencies.serde.version");
+            assert_eq!(
+                err.to_string(),
+                "dependencies.serde.version: invalid type: integer value, expected string value"
+            );
+        }
+    }
+
+    let result: Result<Package, _> = serde_firestore_value::from_value(&value);
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err().to_string(),
+        "invalid type: integer value, expected string value"
+    );
+
+    #[derive(Debug, Eq, PartialEq, serde::Deserialize)]
+    struct Package2 {
+        name: String,
+        dependencies: std::collections::BTreeMap<String, Dependency2>,
+    }
+
+    #[derive(Debug, Eq, PartialEq, serde::Deserialize)]
+    struct Dependency2 {
+        version: i32,
+    }
+
+    let result: Result<Package2, _> = serde_firestore_value::from_value(&value);
+    assert!(result.is_ok());
+    assert_eq!(
+        result.unwrap(),
+        Package2 {
+            name: "demo2".to_string(),
+            dependencies: std::collections::BTreeMap::from_iter([(
+                "serde".to_owned(),
+                Dependency2 { version: 1 }
+            ),]),
+        }
+    );
 }
