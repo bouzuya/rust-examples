ae2f0dad56a203cab59084732f83e8218483acdd
bouzuya <m@bouzuya.net>
2025-11-01 21:08:36 +0900

    axum10: Update

--- axum10/src/main.rs
+++ axum10/src/main.rs
@@ -3,7 +3,7 @@ fn main() {}
 #[cfg(test)]
 mod tests {
     #[tokio::test]
-    async fn test() -> anyhow::Result<()> {
+    async fn test_map_request() -> anyhow::Result<()> {
         async fn handler(header_map: axum::http::HeaderMap) -> String {
             format!("X-NAME: {:?}", header_map.get("X-NAME"))
         }
@@ -31,6 +31,86 @@ mod tests {
             response.into_body_string().await?,
             "X-NAME: Some(\"bouzuya\")"
         );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_map_response() -> anyhow::Result<()> {
+        let app = axum::Router::<()>::new()
+            .route("/", axum::routing::get("Original Body"))
+            .layer(tower::ServiceBuilder::new().map_response(
+                |mut response: axum::http::Response<axum::body::Body>| {
+                    *response.body_mut() = axum::body::Body::from("New Body");
+                    response
+                },
+            ));
+        let response = send_request(
+            app,
+            axum::http::Request::builder()
+                .method("GET")
+                .uri("/")
+                .body(axum::body::Body::empty())?,
+        )
+        .await?;
+        assert_eq!(response.status(), axum::http::StatusCode::OK);
+        assert_eq!(response.into_body_string().await?, "New Body");
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_then() -> anyhow::Result<()> {
+        let app = axum::Router::<()>::new()
+            .route("/", axum::routing::get("Original Body"))
+            .layer(tower::ServiceBuilder::new().then(
+                |result: Result<
+                    axum::response::Response<axum::body::Body>,
+                    std::convert::Infallible,
+                >| async {
+                    match result {
+                        Ok(mut response) => {
+                            *response.body_mut() = axum::body::Body::from("New Body");
+                            Ok(response)
+                        }
+                        Err(e) => Err(e),
+                    }
+                },
+            ));
+        let response = send_request(
+            app,
+            axum::http::Request::builder()
+                .method("GET")
+                .uri("/")
+                .body(axum::body::Body::empty())?,
+        )
+        .await?;
+        assert_eq!(response.status(), axum::http::StatusCode::OK);
+        assert_eq!(response.into_body_string().await?, "New Body");
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_and_then() -> anyhow::Result<()> {
+        let app = axum::Router::<()>::new()
+            .route("/", axum::routing::get("Original Body"))
+            .layer(tower::ServiceBuilder::new().and_then(
+                |mut response: axum::response::Response<axum::body::Body>| async move {
+                    *response.body_mut() = axum::body::Body::from("New Body");
+                    Result::<
+                                axum::response::Response<axum::body::Body>,
+                                std::convert::Infallible,
+                            >::Ok(response)
+                },
+            ));
+        let response = send_request(
+            app,
+            axum::http::Request::builder()
+                .method("GET")
+                .uri("/")
+                .body(axum::body::Body::empty())?,
+        )
+        .await?;
+        assert_eq!(response.status(), axum::http::StatusCode::OK);
+        assert_eq!(response.into_body_string().await?, "New Body");
         Ok(())
     }
 
